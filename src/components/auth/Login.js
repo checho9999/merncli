553d2606b31e3b62bddc79efbd25989e2417e055
Checho <checho9999@gmail.com>
2020-03-11 22:29:15 -0300

    Creando la estructura de navegacion en la app principal con Router, Switch y Route; Creando los componentes Login, NuevaCuenta y Proyectos que seran utilizados inicialmente por el Router; En el componente Login aplicamos estilo, leemos desde el state el input del usuario, y usamos Link para acceder al componente NuevaCuenta; En el componente NuevaCuenta aplicamos estilo, leemos desde el state el input del usuario, y usamos Link para permitir volver hacia Login.

---
+++ src/components/auth/Login.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+
+    //state para actualizar los datos del usuario desde el input
+    const [ usuario, guardarUsuario ] = useState({
+        email: '',
+        password: ''
+    });
+
+    //extraemos los datos del usuario desde el state
+    const { email, password } = usuario;
+
+    //obtenemos todo lo ingresado por el usuario desde el input y lo actualizamos en el state
+    const onChange = e => {
+        guardarUsuario({
+            ...usuario,
+            [ e.target.name ] : e.target.value
+        })
+
+    }
+
+    const onSubmit = e => {
+        //para que no se envie el query string en la parte superior, ni se recarge la pagina
+        e.prevent.default();
+
+    }
+
+    return ( 
+        <div className='form-usuario'>
+            <div className='contenedor-form sombra-dark'>
+                <h1>Iniciar Sesión</h1>
+
+                <form
+                    onSubmit={onSubmit}
+                >
+                    <div className='campo-form'>
+                        <label htmlFor='email'>Email</label>
+                        <input 
+                            type='email'
+                            id='email'
+                            name='email'
+                            placeholder='Tu Email'
+                            value={email}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className='campo-form'>
+                        <label htmlFor='password'>Password</label>
+                        <input 
+                            type='password'
+                            id='password'
+                            name='password'
+                            placeholder='Tu Password'
+                            value={password}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className='campo-form'>
+                        <input type='submit' className='btn btn-primario btn-block' value='Iniciar Sesión' />
+                    </div>
+                </form>
+
+                <Link to={'/nueva-cuenta'} className='enlace-cuenta'>
+                    Obtener Cuenta
+                </Link>
+
+            </div>
+        </div>
+     );
+}
+ 
+export default Login;
---
+++ src/components/auth/NuevaCuenta.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const NuevaCuenta = () => {
+
+    //state para actualizar los datos del usuario desde el input
+    const [ usuario, guardarUsuario ] = useState({
+        nombre: '',
+        email: '',
+        password: '',
+        confirmar: ''
+    });
+
+    //extraemos los datos del nuevo usuario desde el state
+    const { nombre, email, password, confirmar } = usuario;
+
+    //obtenemos todo lo ingresado por el usuario desde el input y lo actualizamos en el state
+    const onChange = e => {
+        guardarUsuario({
+            ...usuario,
+            [ e.target.name ] : e.target.value
+        })
+
+    }
+
+    const onSubmit = e => {
+        //para que no se envie el query string en la parte superior, ni se recarge la pagina
+        e.prevent.default();
+
+    }
+
+
+    return ( 
+        <div className="form-usuario">
+    
+            <div className="contenedor-form sombra-dark">
+                <h1>Obtener una cuenta</h1>
+
+                <form
+                    onSubmit={onSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="nombre">Nombre</label>
+                        <input 
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            placeholder="Tu Nombre"
+                            value={nombre}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu Email"
+                            value={email}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu Password"
+                            value={password}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="confirmar">Confirmar Password</label>
+                        <input 
+                            type="password"
+                            id="confirmar"
+                            name="confirmar"
+                            placeholder="Repite tu Password"
+                            value={confirmar}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block" value="Registrarme" />
+                    </div>
+                </form>
+
+                <Link to={'/'} className="enlace-cuenta">
+                    Volver a Iniciar Sesión
+                </Link>
+            </div>
+        </div>
+     );
+}
+ 
+export default NuevaCuenta;
